d6f869c3e8aadc69c44ea3ecce649e1844f22ca7
unknown <realitystone9876@gmail.com>
2022-04-16 05:33:35 -0300

    Chapter 1 Exercise 3 Text section

---
+++ CH1E3_TEXT/main.cpp
@@ -0,0 +1,23 @@
+//
+
+#include "SSDL.h"
+
+int main(int argc, char** argv)
+{
+	
+	SSDL_SetCursor(0, 50);
+	sout << "\tCharacter\t\tCoolness\n";
+	sout << "\t===========\t===========\n";
+	sout << "\tAstos\t\t100%\n";
+	sout << "\tLeuvre\t\t100%\n";
+	sout << "\tCanal\t\t100%\n";
+	sout << "\tValiant\t\t100%\n";
+	sout << "\tArbo Judas\t\t100%\n";
+	sout << "\tItsuki\t\t50%\n";
+	sout << "\tGada\t\t19%\n";
+	sout << "\tRashana\t\t30%\n";
+	SSDL_WaitKey();
+
+
+   return 0;
+}
