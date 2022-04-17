7fe257f043295725eec9a93fb475b50900115ee6
unknown <realitystone9876@gmail.com>
2022-04-17 06:36:03 -0300

    CH1 EX4 STOP SIGN Part 1

---
+++ CH1E4_STOP_SIGN/main.cpp
@@ -0,0 +1,74 @@
+//
+
+#include "SSDL.h"
+
+int main(int argc, char** argv)
+{
+	//SSDL_RenderDrawLine(10, 50, 50, 10);
+	//SSDL_RenderDrawLine(11, 51, 51, 11);
+
+	for (int i = 0; i < 20; i++)
+	{
+		//SSDL_RenderDrawLine(10 + i, 50 + i, 50 + i, 10 + i);
+		SSDL_RenderDrawLine(10 + i, 50, 50 + i, 10);
+	}
+
+	//SSDL_RenderDrawLine(10, 50, 10, 120);
+
+	for (int j = 0; j < 20; j++)
+	{
+		SSDL_RenderDrawLine(10 + j, 50, 10 + j, 110);
+	}
+
+	//SSDL_RenderDrawLine(10, 110, 50, 150);
+	
+	for (int k = 0; k < 20; k++)
+	{
+		SSDL_RenderDrawLine(10 + k, 110, 50 + k, 150);
+	}
+
+	//SSDL_RenderDrawLine(50, 10, 150, 10);
+
+	for (int l = 0; l < 15; l++)
+	{
+		SSDL_RenderDrawLine(50, 10+l, 130, 10+l);
+	}
+
+	SSDL_RenderDrawLine(50, 150, 130, 150);
+	SSDL_RenderDrawLine(50, 149, 130, 149);
+	for (int m = 0; m < 15; m++)
+	{
+		SSDL_RenderDrawLine(50, 150 - m, 130, 150 - m);
+	}
+
+	//SSDL_RenderDrawLine(130, 150, 170, 115);
+	//SSDL_RenderDrawLine(130, 149, 169, 115);
+
+	for (int b = 0; b < 15; b++)
+	{
+		//SSDL_RenderDrawLine(130, 150 - b, 170 - b, 115);
+		SSDL_RenderDrawLine(130, 150 - b, 165, 115 - b);
+	}
+
+
+	SSDL_RenderDrawLine(130, 10, 165, 45);
+	SSDL_RenderDrawLine(130, 11, 165, 46);
+
+	for (int v = 0; v < 15; v++)
+	{
+		SSDL_RenderDrawLine(130, 10 + v, 165, 45 + v);
+	}
+
+
+	SSDL_RenderDrawLine(165, 45, 165, 115);
+
+	for (int c = 0; c < 15; c++)
+	{
+		SSDL_RenderDrawLine(165 - c, 45, 165 - c, 115);
+	}
+
+
+	SSDL_WaitKey();
+
+   return 0;
+}
